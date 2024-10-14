43b2f7a9d7fcf4d61b1fc63e2de6f4e123cb325b
Ayham7i <ayham1010song@gmail.com>
2024-10-14 23:21:28 +0300

    add learn react env

--- tic_tac_toe/src/App.js
+++ tic_tac_toe/src/App.js
@@ -1,3 +1,16 @@
-export default function Square() {
-  return <button className="square">X</button>;
+function VideoList({ videos, emptyHeading }) {
+  const count = videos.length;
+  let heading = emptyHeading;
+  if (count > 0) {
+    const noun = count > 1 ? 'Videos' : 'Video';
+    heading = count + ' ' + noun;
+  }
+  return (
+    <section>
+      <h2>{heading}</h2>
+      {videos.map(video =>
+        <Video key={video.id} video={video} />
+      )}
+    </section>
+  );
 }
